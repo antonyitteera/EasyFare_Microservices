a178052ef005751d43c84971f3c77a4a585324d5
Antony Itteera <antonyItteera@gmail.com>
2022-03-03 22:34:31 +0000

    user-management module added

---
+++ easyfare_usermanagement/src/main/java/com/tus/easyfare/EasyfareApplication.java
@@ -0,0 +1,14 @@
+package com.tus.easyfare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableEurekaClient
+@SpringBootApplication
+public class EasyfareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyfareApplication.class, args);
+	}
+
+}
---
+++ easyfare_usermanagement/src/test/java/com/tus/easyfare/EasyfareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tus.easyfare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyfareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
